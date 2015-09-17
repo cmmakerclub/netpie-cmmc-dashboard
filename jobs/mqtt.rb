bfcb5fe9057ab1684748717d87ba143dab745327
Nat Weerawan <nat@opendream.co.th>
2015-09-17 13:09:38 +0700

    update

--- jobs/mqtt.rb
+++ jobs/mqtt.rb
@@ -1,26 +1,73 @@
 require 'mqtt'
 
 # Set your MQTT server
-MQTT_SERVER = 'mqtt://localhost'
+MQTT_SERVER = 'rabbit.cmmc.ninja'
 
 # Set the MQTT topics you're interested in and the tag (data-id) to send for dashing events
-MQTT_TOPICS = { 'home/sensors/currentcost/watts' => 'watts',
-                'home/sensors/currentcost/temperature' => 'cctemp',
+MQTT_TOPICS = { 
+               'esp8266/cmmc-temp1/command' => 'temp1',
+               'esp8266/cmmc-humid1/command' => 'humid1',
+               'esp8266/cityview-temp1/command' => 'citytemp1',
+               'esp8266/cityview-humid1/command' => 'cityhumid1',
+               'esp8266/opendream-temp1/command' => 'opendreamtemp1',
+               'esp8266/opendream-humid1/command' => 'opendreamhumid1',
               }
 
+    # send_event('xively_data_temperature', { current: temperature_value })
+    # send_event('xively_data_humidity', { current: humidity_value})
+    # send_event('weather', { current: "CMMC"})
 # Start a new thread for the MQTT client
+print "HELLO"
 Thread.new {
-  MQTT::Client.connect(MQTT_SERVER) do |client|
+  MQTT::Client.connect(
+    :host => MQTT_SERVER,
+    :username => 'nat:dashing',
+    :password => 'dashing'
+  ) do |client|
+    print "BEGIN"
+
     client.subscribe( MQTT_TOPICS.keys )
 
     # Sets the default values to 0 - used when updating 'last_values'
-    current_values = Hash.new(0)
+    # current_values = Hash.new(0)
 
+    print "OK"
     client.get do |topic,message|
+      # print message
       tag = MQTT_TOPICS[topic]
-      last_value = current_values[tag]
-      current_values[tag] = message
-      send_event(tag, { value: message, current: message, last: last_value })
+      # nat = message
+      # print nat
+      # print "\n"
+      # temperature_value =  nat['d']['temp']
+      # humidity_value =  nat['d'][1]['humid']
+
+
+      # print "?"
+      # print tag
+      # print nat['d']['myName']
+      # print temperature_value
+      # last_value = current_values[tag]
+      # current_values[tag] = message
+      # send_event(tag, { value: message, current: message, last: last_value })
+
+      if tag == "temp1"
+        send_event('xively_data_temperature', { current: message})
+      end
+      if tag == "humid1"
+        send_event('xively_data_humidity', { current: message})
+      end
+      if tag == "citytemp1"
+        send_event('cityview_data_temperature', { current: message})
+      end
+      if tag == "cityhumid1"
+        send_event('cityview_data_humidity', { current: message})
+      end
+      if tag == "opendreamtemp1"
+        send_event('opendream_data_temperature', { current: message})
+      end
+      if tag == "opendreamhumid1"
+        send_event('opendream_data_humidity', { current: message})
+      end
     end
   end
 }
--- jobs/xively.rb
+++ jobs/xively.rb
@@ -6,8 +6,8 @@ SCHEDULER.every '2m', :first_in => 0 do |job|
     # datastream = HTTParty.get("https://api.xively.com/v2/feeds/469207408",
     #             { :headers => { 'X-ApiKey' => "EnYoLFD1lVO3YJK3GPzwj3WQ60jcQmSU0Fo0qiN6mYLYD1Ax", "Content-Type" => 'application/json'}})
 
-    nat = HTTParty.get("http://api.thingspeak.com/channels/43022/feed.json?key=7TGNVLL8L74JLD3T",
-                { :headers => { "Content-Type" => 'application/json'}})
+    # nat = HTTParty.get("http://api.thingspeak.com/channels/43022/feed.json?key=7TGNVLL8L74JLD3T",
+    #             { :headers => { "Content-Type" => 'application/json'}})
 
     # datastreams = datastream['datastreams']
      
@@ -19,8 +19,8 @@ SCHEDULER.every '2m', :first_in => 0 do |job|
     # humidity_value = datastreams.find { | ds | ds['id'] ==  "humidity-average" }['current_value']
 
 
-    temperature_value =  nat['feeds'][0]['field1']
-    humidity_value =  nat['feeds'][1]['field1']
+    # temperature_value =  nat['feeds'][0]['field1']
+    # humidity_value =  nat['feeds'][1]['field1']
     # temperature_value = nat['feeds'][0]['field1']
     # humidity_value = nat['feeds'][0]['field2'])
 
@@ -30,8 +30,8 @@ SCHEDULER.every '2m', :first_in => 0 do |job|
     # puts humidity_value
 
     # Bind data to the DOM 
-    send_event('xively_data_temperature', { current: temperature_value })
-    send_event('xively_data_humidity', { current: humidity_value})
-    send_event('weather', { current: "CMMC"})
+    # send_event('xively_data_temperature', { current: temperature_value })
+    # send_event('xively_data_humidity', { current: humidity_value})
+    # send_event('weather', { current: "CMMC"})
   end
 end
